76cae833ddba08257acec5b25e5ccf803756f237
somu biradar <biradarsomu739@gmail.com>
2024-11-18 12:52:23 +0530

    firstcommit

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,24 @@
+import { connect,disconnect } from "mongoose";
+
+ async function connectToDatabase() {
+ try {
+    await connect (process.env.MONGODB_URL);
+
+ } catch (error) {
+    console.log(error);
+    throw new Error("Cannot Connect To Mongodb");
+ }
+}
+ async function disconnectFromDatabase() {
+    try {
+     await disconnect();
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error("Cannot Disconnect To Mongodb");
+        
+    }
+    
+ }
+
+export {connectToDatabase,disconnectFromDatabase};
